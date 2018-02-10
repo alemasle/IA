108a13574cf1f71c07a854f51ff7bac1c014d40d
alemasle <32134441+alemasle@users.noreply.github.com>
2018-02-10 16:54:41 +0100

    maj gitignore

--- TP2/Puissance4/MinMaxExperimental.java
+++ TP2/Puissance4/MinMaxExperimental.java
@@ -8,14 +8,29 @@
  */
 public class MinMaxExperimental {
 
-	public static int miniMax() {
+	public MinMaxExperimental() {
 
-		return 0;
 	}
 
-	public static int maxiMin() {
+	public static double maxiMin(Grille grille) {
+		double m = 0;
+		Grille pred[] = new Grille[7];
 
-		return 0;
+		if (grille.estPleine()) { // si FEUILLE(R) alors alpha <- h(R)
+
+		}
+
+		for (int i = 0; i < 7; i++) { // maximum des evaluations des sous-arbres
+			m = Math.max(m, miniMax(pred[i]));
+		}
+
+		return m; // beta <- min (maximin (succ(R)), maximin (succ(R)), ..., maximin(succ(R)) )
+	}
+
+	public static double miniMax(Grille grille) {
+		double m = 0;
+
+		return m;
 	}
 
 }
