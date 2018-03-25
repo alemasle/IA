236761755cec044bc362b16d586a7647022a5e5b
CaptainMexiko <didotgwendal@gmail.com>
2018-03-25 16:34:09 +0200

    Exercice 1 terminé

--- TP4/Exercice1/Exercice1.py
+++ TP4/Exercice1/Exercice1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+from numpy.random import random
 
 
 def lecsv(datafile):
@@ -11,6 +12,7 @@ def cara(datafile):
     data = pd.read_csv(datafile)
     turns_before_end = 4
     turn_df = data[data["turns_to_end"] == turns_before_end]
+    print(turn_df)
     features_cols = ["my_health", "opponent_health", "my_hand", "opponent_hand", "my_board_nb_creatures",
                      "my_board_total_health", "my_board_total_attack", "opponent_board_nb_creatures",
                      "opponent_board_total_health", "opponent_board_total_attack", "me_playing"]
@@ -22,6 +24,7 @@ def cara(datafile):
     print(X)
     print(Y)
     printrigolo(X, Y)
+    printprecis(X, Y)
 
 
 def printrigolo(X, Y):
@@ -29,8 +32,6 @@ def printrigolo(X, Y):
     y_col = "opponent_health"
     plt.scatter(X[x_col], X[y_col], Y)
     plt.show()
-
-    help(plt.scatter)
     plt.xlabel(x_col)
 
     plt.ylabel(y_col)
@@ -41,6 +42,49 @@ def printrigolo(X, Y):
     plt.show()
 
 
+def printprecis(X, Y):
+    x_col = "my_health"
+    y_col = "opponent_health"
+    print(Y["result"] == 0)
+    print(X[Y["result"] == 0])
+    plt.scatter(X[Y["result"] == 0][x_col], X[Y["result"] == 0][y_col], color="red", label="lose")
+    plt.scatter(X[Y["result"] == 1][x_col], X[Y["result"] == 1][y_col], color="green", label="win")
+    plt.legend()
+    plt.show()
+
+
+def funcfin():
+    data = pd.read_csv("../full_dataset.csv")
+
+    features_cols = ["my_health", "opponent_health", "my_hand", "opponent_hand", "my_board_nb_creatures",
+                     "my_board_total_health", "my_board_total_attack", "opponent_board_nb_creatures",
+                     "opponent_board_total_health", "opponent_board_total_attack", "me_playing"]
+
+    pred_col = ["result"]
+
+    turns_before_end = 4
+
+    turn_df = data[data["turns_to_end"] == turns_before_end]
+    X = turn_df[features_cols]
+    Y = turn_df[pred_col]
+
+    x_col = "my_health"
+    y_col = "opponent_health"
+
+    colors = ["red", "green"]
+    for i in range(2):
+        indices = Y["result"] == i
+        plt.scatter(X[indices][x_col] + 0.5 * (random(X[indices].shape[0]) - 0.5),
+                    X[indices][y_col] + 0.5 * (random(X[indices].shape[0]) - 0.5),
+                    color=colors[i], label=i, s=2)
+    plt.legend()
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.title("Hearthstone data: health")
+    plt.show()
+
+
 if __name__ == '__main__':
     data_file = "../full_dataset.csv"
-    cara(data_file)
+    #cara(data_file)
+    funcfin()
--- TP4/Exercice1/ReponseExo1.md
+++ TP4/Exercice1/ReponseExo1.md
@@ -12,3 +12,16 @@ Q6 : Affiche la vie du joueur pour les 36148 tours
 
 Q7 : Affiche les pv et nbr de cartes du joueur pour les 5 premier tours
 
+Q8 : Donne tous les tours en mettant les tours ou il rest 4 tours avant la fin
+
+Q9 : Créer une répartition des parties en fonction de la vie du joueur et de l'adversaire
+
+Q10 : affiche la répartiton
+
+Q11 : ajoute un nom a la colonne dans l'affichage
+
+Q12 : Affiche toutes les colonnes de tous les tours ou le joueur a perdu
+
+Q13 : Répartition les tours où le joueur à perdu en fonction des points de vie des deux joueurs
+
+Q14 : Affiche la signification des points (win/lose)
