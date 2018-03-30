84db38703f3830b34acd6c4e83649adc811aee3e
CaptainMexiko <didotgwendal@gmail.com>
2018-03-30 17:22:30 +0200

    ajout de l'affichage du score en graph

--- TP4/Exercice3/TP4prog5.py
+++ TP4/Exercice3/TP4prog5.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as plt
 import random
 
 
@@ -11,7 +11,12 @@ def get_data(input_file):
 
 
 def make_tree(data):
-    for i in range(1, 51):
+    scores_train = []
+    scores_test = []
+    name = range(1, 51)
+    for i in name:
+        score_train = 0
+        score_test = 0
         ret_tree = tree.DecisionTreeClassifier(max_leaf_nodes=50 * i)
 
         features_cols = ["my_health", "opponent_health", "my_hand", "opponent_hand", "my_board_nb_creatures",
@@ -27,12 +32,15 @@ def make_tree(data):
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=random.seed())
 
         ret_tree = ret_tree.fit(X_train, Y_train)
-
-        Y_pred = ret_tree.predict(X_test)
-        cm = confusion_matrix(Y_test, Y_pred)
-        print(cm)
+        score_train = ret_tree.score(X_train, Y_train)
+        score_test = ret_tree.score(X_test, Y_test)
+        scores_test.append(score_test)
+        scores_train.append(score_train)
         tree.export_graphviz(ret_tree, out_file="dot_prog5/tree{}.dot".format(i), feature_names=features_cols)
-
+    plt.plot(name, scores_train)
+    plt.show()
+    plt.plot(name, scores_test)
+    plt.show()
 
 if __name__ == '__main__':
     data = get_data("../full_dataset.csv")
